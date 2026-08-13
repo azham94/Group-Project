722d800dafd60e5e5555d77f86ec0c0998e5573b
AhmadFakhrusy <2024642602@student.uitm.edu.my>
2026-08-13 18:12:34 +0800

    Update server.c

--- Group Project/c_server2/server.c
+++ Group Project/c_server2/server.c
@@ -1,44 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <arpa/inet.h>
+#include <pthread.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <mysql/mysql.h>
 
-int main()
-{
-    int server_fd;
-    int client_socket;
-
-    struct sockaddr_in server_addr;
-
-    char message[]="100";
-
-    server_fd=socket(AF_INET,SOCK_STREAM,0);
-
-    server_addr.sin_family=AF_INET;
-
-    server_addr.sin_port=htons(6002);
-
-    server_addr.sin_addr.s_addr=INADDR_ANY;
-
-    bind(server_fd,
-         (struct sockaddr*)&server_addr,
-         sizeof(server_addr));
-
-    listen(server_fd,5);
-
-    printf("C Server2 Running\n");
-
-    while(1)
-    {
-        client_socket=accept(server_fd,NULL,NULL);
-
-        send(client_socket,
-             message,
-             strlen(message),
-             0);
-
-        close(client_socket);
+void* db_updater(void* arg) {
+    while(1) {
+        MYSQL *conn = mysql_init(NULL);
+        if (mysql_real_connect(conn, "database-service-mysql", "root", "secret", "itt440_db", 3306, NULL, 0)) {
+            mysql_query(conn, "UPDATE game_scores SET points = points + 5, datetime_stamp = NOW() WHERE user = 'fakhrusy';");
+            mysql_close(conn);
+        }
+        sleep(30);
     }
+    return NULL;
+}
 
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    char *response = "HTTP/1.1 200 OK\nContent-Type: text/plain\n\nFakhrusy Server Active\n";
+
+    pthread_t thread_id;
+    pthread_create(&thread_id, NULL, db_updater, NULL);
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(5001);
+
+    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+    listen(server_fd, 3);
+
+    while((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))) {
+        read(new_socket, buffer, 1024);
+        send(new_socket, response, strlen(response), 0);
+        close(new_socket);
+    }
     return 0;
 }
